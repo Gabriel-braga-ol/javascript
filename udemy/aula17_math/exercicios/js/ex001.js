37f8bbeda3fee5b94a630172fef67fee539c9ab0
Gabriel Braga <bielbraga37@outlook.com>
2026-07-20 16:09:50 -0300

    ajustei o exercício de Math

--- udemy/aula17_math/exercicios/js/ex001.js
+++ udemy/aula17_math/exercicios/js/ex001.js
@@ -2,28 +2,23 @@ const numero = Number(prompt('Digite um número: ')); // prompt sempre retorna u
 
 const numeroTitulo = document.getElementById('numero-titulo');
 
-const raizQuadrada = document.getElementById('raiz-quadrada');
+const texto = document.getElementById('texto');
 
-const inteiro = document.getElementById('inteiro');
+// const inteiro = document.getElementById('inteiro');
 
-const temp = document.getElementById('not-a-number');
+// const temp = document.getElementById('not-a-number');
 
-const num1 = document.getElementById('round-floor');
+// const num1 = document.getElementById('round-floor');
 
-const num2 = document.getElementById('round-ceil');
+// const num2 = document.getElementById('round-ceil');
 
-const num3 = document.getElementById('decimal');
+// const num3 = document.getElementById('decimal');
 
 numeroTitulo.innerHTML = numero;
 
-raizQuadrada.innerHTML = `<p>Raiz quadrada: ${Math.sqrt(numero)}.</p>`;
-
-inteiro.innerHTML = `<p>${numero} é inteiro? ${Number.isInteger(numero)}</P>`;
-
-temp.innerHTML = `<p>É NaN? ${Number.isNaN(numero)}</P>`;
-
-num1.innerHTML = `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`;
-
-num2.innerHTML = `<p>Arredondando para cima: ${Math.ceil(numero)}</p>`;
-
-num3.innerHTML = `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`;
+texto.innerHTML = '';
+texto.innerHTML += `<p>${numero} é inteiro? ${Number.isInteger(numero)}</P>`;
+texto.innerHTML += `<p>É NaN? ${Number.isNaN(numero)}</P>`;
+texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}</p>`;
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`;
