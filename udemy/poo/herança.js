f5564f24d8778857699bc7a4b9946eb91b80d7b1
Gabriel Braga <bielbraga37@outlook.com>
2026-08-31 19:46:07 -0300

    herança com classes

---
+++ udemy/poo/herança.js
@@ -0,0 +1,50 @@
+class DispositivoEletronico {
+    constructor(nome) {
+        this.nome =nome
+        this.ligado = false
+    }
+
+    ligar() {
+        if (this.ligado) {
+            console.log(this.nome + 'já está ligado')
+            return
+        }
+        this.ligado = true
+    }
+
+    desligar() {
+        if (!this.ligado) {
+            console.log(this.nome + 'já está desligado')
+            return
+        }
+
+        this.ligado = false
+    }
+}
+
+class Smartphone extends DispositivoEletronico {
+    constructor(nome, cor, modelo) {
+        super(nome)
+        this.cor = cor
+        this.modelo = modelo
+    }
+} // classe Smartphone herda de DispositivoEletronico
+
+class Tablet extends DispositivoEletronico {
+    constructor(nome, temWifi) {
+        super(nome)
+        this.temWifi = temWifi
+    }
+
+    ligar() {
+        console.log('Você alterou o método ligar')
+    }
+}
+
+const t1 = new Tablet('Samsung', true)
+t1.ligar()
+console.log(t1)
+
+
+
+
