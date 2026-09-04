62bf0af90593fc4db3f8fef6b2e45a8531190ad7
Gabriel Braga <bielbraga37@outlook.com>
2026-09-04 01:05:13 -0300

    promise

---
+++ udemy/assincrono/promise.js
@@ -0,0 +1,29 @@
+function rand(min, max) {
+    min *= 1000
+    max *= 1000
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        if(typeof msg !== 'string') reject('bad value')
+
+        setTimeout(() => {
+            resolve(msg);
+        }, tempo)
+    })
+
+}
+
+esperaAi('frase 1', rand(1, 3)).then(resposta => {
+    console.log(resposta)
+    return esperaAi(22222, rand(1, 3))
+}).then(resposta => {
+    console.log(resposta)
+    return esperaAi('frase 3', rand(1, 3))
+}).then(resposta => {
+    console.log(resposta)
+})
+.catch(e => {
+    console.log('error:', e)
+})
